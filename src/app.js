50d559d9983ef3c1bdf4838e60f4ce07b8ba4f07
Joanna <96662133+Joanna32@users.noreply.github.com>
2022-02-05 18:58:53 +0100

    add weather icon from API

--- src/app.js
+++ src/app.js
@@ -22,7 +22,10 @@ function formatDate(timestamp) {
 }
 
 function showCurrentConditions(response) {
+  console.log(response.data);
   let inputCity = document.querySelector("#city");
+  let weatherIcon = document.querySelector("#icon");
+
   inputCity.innerHTML = response.data.name;
   document.querySelector("#current-date").innerHTML = formatDate(
     response.data.dt * 1000
@@ -36,8 +39,13 @@ function showCurrentConditions(response) {
   document.querySelector("#current-temp").innerHTML = Math.round(
     response.data.main.temp
   );
+  weatherIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  weatherIcon.setAttribute("alt", response.data.weather[0].description);
 }
-let city = "Dublin";
+let city = "London";
 let apiKey = "5c65b0445be84c47d8d9f65d36c11cc2";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(showCurrentConditions);
