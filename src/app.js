80ef32dd6d4fb29395f2a2125a0437f90b3be571
Joanna <96662133+Joanna32@users.noreply.github.com>
2022-02-05 11:20:37 +0100

    add weather conditions

--- src/app.js
+++ src/app.js
@@ -1,2 +1,44 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function showCurrentConditions(response) {
+  console.log(response.data);
+  let inputCity = document.querySelector("#city");
+  inputCity.innerHTML = response.data.name;
+  document.querySelector("#current-date").innerHTML = formatDate(
+    response.data.dt * 1000
+  );
+  document.querySelector("#weather-conditions").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#current-temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+let city = "Munich";
 let apiKey = "5c65b0445be84c47d8d9f65d36c11cc2";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Dublin&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(showCurrentConditions);
